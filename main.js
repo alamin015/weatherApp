7e637d21e8dbe237cf48458ee4330b077fc565c6
Alamin <alaminsf40@gmail.com>
2021-12-28 23:01:32 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const btn = document.querySelector(".btn")
+// const input = document.getElementById("input")
+
+
+btn.addEventListener("click", () => {
+    const inputValue = commonFunction("input");
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=c42756cbd053d1ee1148b053de09fcd6`;
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => weatherData(data))
+
+    // console.log(inputValue)
+})
+
+
+
+
+const commonFunction = (id) => {
+    const h = document.getElementById(id).value;
+    return h;
+}
+
+const weatherData = (data) => {
+    
+    const situation = data.weather[0].main;
+    const cel = parseInt(data.main.temp - 273);
+    getHTMLData("state",situation)
+    getHTMLData("deg",cel)
+    getHTMLData("city",data.name)
+}
+
+
+// const get innerHTML data  by function 
+
+const getHTMLData = (id,city) => {
+    const value = document.getElementById(id)
+    value.innerHTML = city;
+}
